4e170695e50b51799af7c71ec8ee2d75165b56ae
Wesley Howell <wesleyrhowell@gmail.com>
2022-08-28 16:23:39 -0500

    Update info

--- Blog/Blog/ClientApp/src/Components/AboutPage.js
+++ Blog/Blog/ClientApp/src/Components/AboutPage.js
@@ -1,67 +1,75 @@
-import React from "react";
-import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import {
   VerticalTimeline,
   VerticalTimelineElement,
-} from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
-import WorkIcon from "@material-ui/icons/Work";
-import SchoolIcon from "@material-ui/icons/School";
-import StarIcon from "@material-ui/icons/Star";
-import AvatarImage from "../Images/profilepicture.jpg";
-import OuLogo from "../Images/oulogo.png";
-import KuLogo from "../Images/kulogo.png";
-import AbfLogo from "../Images/abf.png";
-import GarminLogo from "../Images/garmin.svg";
-import CpLogo from "../Images/cplogo.png";
-import WesLogo from "../Images/icon.png";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import GitHubIcon from "@material-ui/icons/GitHub";
-import WebIcon from "@material-ui/icons/Web";
-import { aboutMe } from "../Content/AboutMe";
-import ExperienceIconPanel from "./ExperienceIconPanel";
+} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import WorkIcon from '@material-ui/icons/Work';
+import SchoolIcon from '@material-ui/icons/School';
+import StarIcon from '@material-ui/icons/Star';
+import AvatarImage from '../Images/profilepicture.jpg';
+import OuLogo from '../Images/oulogo.png';
+import KuLogo from '../Images/kulogo.png';
+import AbfLogo from '../Images/abf.png';
+import GarminLogo from '../Images/garmin.svg';
+import CpLogo from '../Images/cplogo.png';
+import TmLogo from '../Images/tmlogo.jpg';
+import WesLogo from '../Images/icon.png';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import WebIcon from '@material-ui/icons/Web';
+import {aboutMe} from '../Content/AboutMe';
+import ExperienceIconPanel from './ExperienceIconPanel';
 
 export default function Album() {
   const returnLogo = (item) => {
-    if (item.logo === "OuLogo") {
+    if (item.logo === 'OuLogo') {
       return (
-        <div style={{ width: "50px" }}>
+        <div style={{width: '50px'}}>
           <img src={OuLogo} alt="" width="100%" />
         </div>
       );
     }
-    if (item.logo === "KuLogo") {
+    if (item.logo === 'KuLogo') {
       return (
-        <div style={{ width: "50px" }}>
+        <div style={{width: '50px'}}>
           <img src={KuLogo} alt="" width="100%" />
         </div>
       );
     }
-    if (item.logo === "ArcBLogo") {
+    if (item.logo === 'ArcBLogo') {
       return (
-        <div style={{ width: "80px" }}>
+        <div style={{width: '80px'}}>
           <img src={AbfLogo} alt="" width="100%" />
         </div>
       );
     }
-    if (item.logo === "GarminLogo") {
+    if (item.logo === 'GarminLogo') {
       return (
-        <div style={{ width: "75px" }}>
+        <div style={{width: '75px'}}>
           <img src={GarminLogo} alt="" width="100%" />
         </div>
       );
     }
-    if (item.logo === "CpLogo") {
+    if (item.logo === 'CpLogo') {
       return (
-        <div style={{ width: "50px" }}>
+        <div style={{width: '50px'}}>
           <img src={CpLogo} alt="" width="100%" />
         </div>
       );
     }
+    if (item.logo === 'TmLogo') {
+      return (
+        <div style={{width: '50px'}}>
+          <img src={TmLogo} alt="" width="100%" />
+        </div>
+      );
+    }
     return (
-      <div style={{ width: "50px" }}>
+      <div style={{width: '50px'}}>
         <img src={WesLogo} alt="" width="100%" />
       </div>
     );
@@ -70,11 +78,11 @@ export default function Album() {
     return (
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        contentStyle={{ background: "slategrey", color: "#fff" }}
+        contentStyle={{background: 'slategrey', color: '#fff'}}
         contentArrowStyle={{
-          borderRight: "7px solid  slategrey",
+          borderRight: '7px solid  slategrey',
         }}
-        iconStyle={{ background: "steelblue", color: "#fff" }}
+        iconStyle={{background: 'steelblue', color: '#fff'}}
         icon={<WebIcon />}
       >
         <Grid container spacing={2}>
@@ -102,11 +110,11 @@ export default function Album() {
     return (
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        contentStyle={{ background: "slategrey", color: "#fff" }}
+        contentStyle={{background: 'slategrey', color: '#fff'}}
         contentArrowStyle={{
-          borderRight: "7px solid  slategrey",
+          borderRight: '7px solid  slategrey',
         }}
-        iconStyle={{ background: "slategrey", color: "#fff" }}
+        iconStyle={{background: 'slategrey', color: '#fff'}}
         icon={<WorkIcon />}
       >
         <Grid container spacing={2}>
@@ -133,7 +141,7 @@ export default function Album() {
     return (
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        iconStyle={{ background: "slategrey", color: "#fff" }}
+        iconStyle={{background: 'slategrey', color: '#fff'}}
         icon={<WorkIcon />}
       >
         <Grid container spacing={2}>
@@ -160,7 +168,7 @@ export default function Album() {
     return (
       <VerticalTimelineElement
         className="vertical-timeline-element--education"
-        iconStyle={{ background: "darkred", color: "#fff" }}
+        iconStyle={{background: 'darkred', color: '#fff'}}
         icon={<SchoolIcon />}
       >
         <Grid container spacing={2}>
@@ -184,13 +192,13 @@ export default function Album() {
 
   const renderTimelineHistory = (items) => {
     return aboutMe.history.map((item) => {
-      if (item.type === "web") {
+      if (item.type === 'web') {
         return renderWebHistoryItem(item);
-      } else if (item.type === "work" && item.current) {
+      } else if (item.type === 'work' && item.current) {
         return renderCurrentWorkItem(item);
-      } else if (item.type === "work" && !item.current) {
+      } else if (item.type === 'work' && !item.current) {
         return renderWorkHistoryItem(item);
-      } else if (item.type === "school") {
+      } else if (item.type === 'school') {
         return renderSchoolItem(item);
       } else {
         return <React.Fragment />;
@@ -224,7 +232,7 @@ export default function Album() {
               src={AvatarImage}
               alt=""
               width="40%"
-              style={{ borderRadius: "50%", border: "5px solid #fff" }}
+              style={{borderRadius: '50%', border: '5px solid #fff'}}
             />
             <p></p>About Me
           </Typography>
@@ -245,7 +253,7 @@ export default function Album() {
                   color="primary"
                   target="_blank"
                   href={aboutMe.contact.linkedIn}
-                  style={{ backgroundColor: "slategrey" }}
+                  style={{backgroundColor: 'slategrey'}}
                 >
                   <LinkedInIcon />
                   &nbsp;LinkedIn
@@ -257,7 +265,7 @@ export default function Album() {
                   color="primary"
                   target="_blank"
                   href={aboutMe.contact.github}
-                  style={{ backgroundColor: "slategrey" }}
+                  style={{backgroundColor: 'slategrey'}}
                 >
                   <GitHubIcon />
                   &nbsp;GitHub
@@ -272,7 +280,7 @@ export default function Album() {
           <VerticalTimeline>
             {renderTimelineHistory()}
             <VerticalTimelineElement
-              iconStyle={{ background: "steelblue", color: "#fff" }}
+              iconStyle={{background: 'steelblue', color: '#fff'}}
               icon={<StarIcon />}
             />
           </VerticalTimeline>
